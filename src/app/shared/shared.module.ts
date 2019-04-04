66daa1eecbb86399124234efabbcd633d342c973
Julien Castagne <jcastagne@cpaglobal.com>
2019-04-04 17:12:57 +0200

    Enabling sass styling

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {MatTableModule} from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [MatTableModule],
+  exports: [MatTableModule],
+  providers: [],
+})
+export class SharedModule {}
