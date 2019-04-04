3335f94c9e4b8ebd758eb348467538c866cc2f07
Julien Castagne <jcastagne@cpaglobal.com>
2019-04-04 17:15:41 +0200

    Updating packages : - Adding Bootstrap - Adding Angular Material 2 (& its dependancies : HammerJs and BrowserAnimationsModule ) along with some theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PlantListComponent } from './plants/plant-list/plant-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { PlantListComponent } from './plants/plant-list/plant-list.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
