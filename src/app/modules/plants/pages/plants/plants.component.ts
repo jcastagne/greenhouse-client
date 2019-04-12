decab658ad20dfbc87d68245baab5f91f44801d3
Julien Castagne <jcastagne@cpaglobal.com>
2019-04-12 12:17:26 +0200

    Handling click event on md-chips

--- src/app/modules/plants/pages/plants/plants.component.ts
+++ src/app/modules/plants/pages/plants/plants.component.ts
@@ -7,6 +7,7 @@ import { Plant } from '../../shared/plant';
 import { Tag } from 'src/app/shared/models/tag.model';
 import { TagService } from 'src/app/services/tag/tag.service';
 import { PlantDialogComponent } from '../../components/plant-dialog/plant-dialog.component';
+import { isUndefined, isNullOrUndefined } from 'util';
 
 @Component({
   templateUrl: './plants.component.html'
@@ -19,23 +20,23 @@ export class PlantsComponent implements OnInit {
   primaryTags: Tag[];
   tags: Tag[];
 
-  private pListFilter: string;
+  private pListFilter = '';
   get listFilter(): string {
     return this.pListFilter;
   }
   set listFilter(value: string) {
     // Once linked to the API, should consider using a Search button for performance
     this.pListFilter = value;
-    this.filteredPlants = this.listFilter ? this.performFilter() : this.plants;
+    this.filteredPlants = isNullOrUndefined(this.listFilter) ? this.plants : this.performFilter();
   }
 
-  private ptagFilter: string[];
+  private pTagFilter: string[] = [];
   get tagFilter(): string[] {
-    return this.ptagFilter;
+    return this.pTagFilter;
   }
   set tagFilter(value: string[]) {
     // Once linked to the API, should consider using a Search button for performance
-    this.ptagFilter = value;
+    this.pTagFilter = value;
     this.filteredPlants = this.performFilter();
   }
   //#endregion
@@ -54,10 +55,8 @@ export class PlantsComponent implements OnInit {
       this.arrayContains(plant.tags.map(tag => tag.value), this.tagFilter));
   }
 
-  openDetailsDialog(plantId: number): void {
-    // TODO: remove debug log
-    console.info(`Id : ${plantId} clicked in PlantsComponent`);
-
+  onDetailsClicked(plantId: number): void {
+    // Opens details dialog
     this.dialog.open(PlantDialogComponent, {
       data: {
         plant: this.plants.find(plant => plant.id === plantId)
@@ -67,16 +66,16 @@ export class PlantsComponent implements OnInit {
 
   onFavoriteClicked(plantId: number): void {
     const idx = this.plants.findIndex(plant => plant.id === plantId);
-
     this.plants[idx].favorite = !this.plants[idx].favorite;
-
-    // TODO: remove debug log
-    console.info(`Id: ${plantId} favorite cliked. New value : ${this.plants[idx].favorite}`);
-
   }
 
   onChangeSelectedTagFilter(value): void {
     this.tagFilter = value.value;
+  }
+
+  onTagFilterClicked(chip): void {
+    const length = !!chip.selected ? this.pTagFilter.push(chip.value) : this.pTagFilter.splice(this.tagFilter.indexOf(chip.value), 1);
+    this.filteredPlants = this.performFilter();
   }
   //#endregion
 
@@ -95,10 +94,9 @@ export class PlantsComponent implements OnInit {
   ngOnInit() {
     // Get Hard-coded calues from service waiting to get them from API
     this.plants = this.plantService.getPlants();
-    this.listFilter = '';
+    this.filteredPlants = this.plants;
     this.tags = this.tagService.getTags();
     this.primaryTags = this.tagService.getPrimaryTags();
-    this.tagFilter = [];
   }
   //#endregion
 
@@ -106,6 +104,10 @@ export class PlantsComponent implements OnInit {
   //#region  private methods
 
   arrayContains(array, filterArray): boolean {
+
+    if (isNullOrUndefined(filterArray)) {
+      return true;
+    }
     for (let element of filterArray) {
       if (array.indexOf(element) === -1) {
         // Returns false as soon as an element filterArray is not in array
